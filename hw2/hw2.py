7e02953b4b1261371e400fbd9878cb5fd2febe35
caryannm <108145275+caryannm@users.noreply.github.com>
2022-10-08 10:44:12 -0700

    initial hw2 and hw2 data

---
+++ hw2/hw2.py
@@ -0,0 +1,20 @@
+# Given a file that contains Tab delimited text data, 
+# write a program that:
+# 1.Reads a filename as an argument
+# 2.Then, opens that file, and reads all the data from 
+# the file, then converts it to one of the following 
+# formats, based on a second argument:
+#       -c   CSV format
+#       -j  JSON format
+#       -x  XML format
+# 3. And saves a new file, with the appropriate file 
+# format in the current directory.
+# i.e., the user selects a letter, and the file is 
+# saved in the appropriate format.
+# Upload word doc showing the execution, with a link to 
+# your source code, and the 3 files (one each of CSV, 
+# JSON, and XML) created by your program.
+
+import csv
+
+f = open("")
